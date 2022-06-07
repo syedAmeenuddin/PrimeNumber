2cad595ee635a2340fbb1356339ea02a63402a29
Syed Ameenuddin <ameen.syed124@gmail.com>
2022-06-07 22:02:36 +0530

    Update primeNumber.py

--- primeNumber.py
+++ primeNumber.py
@@ -1,12 +1,12 @@
 # primeNumber Logic => divisible by its own and by 1
 
 # To check the number is prime or not
-number=int(input("check prime or number"))
+number=int(input("check prime number or not"))
 flag1=False
 for i in range(2,number):
   if (number%i)==0:
     flag1 = True
-if flag1=False:
+if flag1==False:
   print(f'{number} is an Prime Number')
 else:
   print(f'{number} is not an Prime Number')
@@ -25,7 +25,6 @@ while count!=check_nth_number+1:
         if (num%i)==0:
             flag2=True            
     if flag2==False:
-        print(f'{num} Its an prime Number')
         count+=1
     if count==check_nth_number:
         print(f'{num} is an {count}th prime Number')
